5b3661f80c79cc84605175a3b6b90a2d7b779de7
actapia <andrew.tapia@uky.edu>
2023-08-24 02:35:05 -0400

    Working parallel version.

--- generate_simulated_data.py
+++ generate_simulated_data.py
@@ -5,8 +5,11 @@ import glob
 import functools
 import contextlib
 import logging
+import multiprocessing
 from pathlib import Path
 from collections import Counter
+
+from joblib import Parallel, delayed
 
 from tqdm import tqdm
 
@@ -19,7 +22,7 @@ from scipy import stats
 
 from dendropy.simulate import treesim
 from dendropy.model import discrete
-from dendropy.utility import GLOBAL_RNG as DENDROPY_RNG
+import dendropy.utility
 
 from Bio import SeqIO
 from Bio.Seq import Seq
@@ -29,22 +32,55 @@ def generate_sequences(
         tree,
         seq_len_dist,
         generate_chars,
-        count=1
+        np_rand,
+        count=1,
+        jobs=1
 ):
     # lens = seq_len_dist.rvs(size=count)
-    for _ in tqdm(range(count)):
-        #embed()
-        dna_chars = generate_chars(seq_len_dist(), tree)
-        yield dna_chars
+    seeds = np_rand.randint(np.iinfo(np.int32).max, size=count)
+    print(seeds)
+    # for _ in tqdm(range(count)):
+    #     #embed()
+    #     dna_chars = generate_chars(seq_len_dist(), tree)
+    #     yield dna_chars
+    # gs_seeded = seeded(
+    #     functools.partial(gen_seq, generate_chars, tree, seq_len_dist)
+    # )
+    return Parallel(n_jobs=jobs, backend="loky", return_generator=True)(
+            delayed(
+                #gs_seeded
+                sgs
+            )(generate_chars, tree, seq_len_dist, seed)
+        for seed in tqdm(seeds)
+    )
+
+def sgs(generate_chars, tree, seq_len_dist, seed):
+    #seed_random(seed)
+    #return gen_seq(generate_chars, tree, seq_len_dist)
+    seed = int(seed)
+    rand = random.Random()
+    rand.seed(seed)
+    dendropy.utility.GLOBAL_RNG = None
+    np_rand = np.random.RandomState(seed=seed+1)
+    res = generate_chars(
+        seq_len_dist(py_rand=rand, np_rand=np_rand),
+        tree,
+        py_rand=rand,
+        np_rand=np_rand
+    )
+    #print("res is {}".format(type(res)))
+    return list(res.values())
+
+def gen_seq(generate_chars, tree, seq_len_dist):
+    res = generate_chars(seq_len_dist(), tree)
+    #assert res is not None
+    return list(res.values())
 
 def seeded(f):
-    def inner(*args, seed, **kwargs):
+    def inner(seed, *args, **kwargs):
         seed_random(seed)
-        f(*args, **kwargs)
+        return f(*args, **kwargs)
     return inner
-
-# New idea: Use a configuration file to control generation.
-# Some parameters could also be optionally controlled via command line args.
 
 def int_or_str(valid):
     def inner(v):
@@ -90,6 +126,11 @@ def handle_arguments():
         "--log-level",
         choices=log_levels
     )
+    parser.add_argument(
+        "--jobs",
+        "-j",
+        type=int
+    )
     return parser.parse_args()
 
 default_config = {
@@ -98,7 +139,8 @@ default_config = {
     "log_level": "WARNING",
     "log_transcript_every": 1000,
     "seqid_template": "transcript_{gene}",
-    "separate_dirs": False
+    "separate_dirs": False,
+    "jobs": 1
 }
 
 def get_config(args, set_logging=True):
@@ -137,6 +179,8 @@ def get_config(args, set_logging=True):
             "fit_data",
             config.get("fit_data")
         )
+    if config["jobs"] <= 0:
+        config["jobs"] = ((config["jobs"] - 1)%multiprocessing.cpu_count()) + 1
     return config
 
 def preset(f):
@@ -144,10 +188,10 @@ def preset(f):
         return functools.partial(f, **kwargs)
     return inner
 
-def weighted_choice(weight_dict):
+def weighted_choice(weight_dict, rng):
     # Thanks to pbsds on Stack Overflow for this trick.
     # https://stackoverflow.com/a/54494915
-    return random.choices(*zip(*weight_dict.items()))[0]
+    return rng.choices(*zip(*weight_dict.items()))[0]
 
 def match_dist(fit_data):
     return functools.partial(
@@ -157,39 +201,58 @@ def match_dist(fit_data):
         )
     )
 
+
+def use_np(f, argname="random_state"):
+    def inner(*args, py_rand, np_rand, **kwargs):
+        kwargs[argname] = np_rand
+        return f(*args, **kwargs)
+    return inner
+
+def use_py(f, argname="rng"):
+    def inner(*args, py_rand, np_rand, **kwargs):
+        kwargs[argname] = py_rand
+        return f(*args, **kwargs)
+    return inner
+
 tree_generators = {
-    "birth_death_tree": preset(treesim.birth_death_tree)
+    "birth_death_tree": preset(use_py(treesim.birth_death_tree))
 }
 
 char_generators = {
-    "hky85": preset(discrete.hky85_chars)
+    "hky85": preset(use_py(discrete.hky85_chars))
 }
 
 seq_len_distributions = {
-    "binomial": lambda kwargs: stats.binom(**kwargs).rvs,
-    "match": lambda kwargs: match_dist(**kwargs)
+    "binomial": lambda kwargs: use_np(stats.binom(**kwargs).rvs),
+    "match": lambda kwargs: use_py(match_dist(**kwargs))
 }
 
 def load_from_specs(d, c):
+    #embed()
     return d[c["name"]](c.get("params", {}))
 
 def count_sequences(fit_data):
     return max(sum(1 for _ in SeqIO.parse(f, format="fasta")) for f in fit_data)
 
 def seed_random(seed):
+    seed = int(seed)
     random.seed(seed)
     np.random.seed(seed + 1)
     DENDROPY_RNG.seed(seed + 2)
 
 def main():
     config = get_config(handle_arguments())
+    rand = random.Random()
     # Set random seed if provided.
     if "seed" in config:
-        seed_random(config["seed"])
+        rand.seed(config["seed"])
+        np_rand = np.random.RandomState(seed=config["seed"]+1)
+    else:
+        np_rand = np.random.RandomState()
     config["out_dir"].mkdir(exist_ok=True)
     tree_gen = load_from_specs(tree_generators, config["tree_generator"])
     logging.debug("Generating tree.")
-    tree = tree_gen()
+    tree = tree_gen(py_rand=rand, np_rand=np_rand)
     logging.debug(tree.as_ascii_plot())
     if "save_tree" in config:
         with open(Path(config["out_dir"])/config["save_tree"], "w") as out_file:
@@ -228,13 +291,16 @@ def main():
                     tree,
                     seq_len_dist,
                     char_gen,
-                    config["count"]
+                    np_rand,
+                    config["count"],
+                    jobs=config["jobs"]
                 )
         ):
+            #embed()
             # if i%config["log_transcript_every"] == 0:
             #     logging.info(f"Writing transcript {i}")
             try:
-                cov = random.uniform(
+                cov = rand.uniform(
                     config["random_cov"].get("min", 0),
                     config["random_cov"].get("max", 10000)
                 )
@@ -248,7 +314,7 @@ def main():
                 iso=1
             )
             #embed()
-            for f, seq in zip(files, mat.values()):
+            for f, seq in zip(files, mat):
                 SeqIO.write(
                     SeqRecord(Seq(str(seq)), id=seq_id, description=""),
                     f,
